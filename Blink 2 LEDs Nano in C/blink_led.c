bf43de2fb70885b2d74810d120a27111ba6908f4
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-03-26 18:48:57 +0200

    Update blink_led.c

--- Blink 2 LEDs Nano in C/blink_led.c
+++ Blink 2 LEDs Nano in C/blink_led.c
@@ -1,1 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
 
+int main(){
+    DDRB = DDRB | (1 << DDB1);
+    DDRB = DDRB | (1 << DDB2);
+
+
+    while(1){
+        PORTB = PORTB | (1 << PORTB1);
+        PORTB = PORTB & ~(1 << PORTB2);
+        _delay_ms(10000);
+        PORTB = PORTB & ~(1 << PORTB1);
+        PORTB = PORTB | (1 << PORTB2);
+        _delay_ms(10000);
+    }
+
+    return 0;
+}
