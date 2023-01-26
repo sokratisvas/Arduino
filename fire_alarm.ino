9f9afa98ef7227eb9faa172e3a057343aaa7e7be
Sokratis Vasiliou <sokratisvasiliou@protonmail.com>
2023-01-26 12:49:44 +0200

    Clean up fire_alarm.ino

--- fire_alarm.ino
+++ fire_alarm.ino
@@ -3,31 +3,26 @@ const int flamePin = 11;
 int Flame = HIGH;
 int redled = 5;
 int greenled = 6;
-void setup() 
-{
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(redled, OUTPUT);
-  pinMode(greenled, OUTPUT);
 
-  pinMode(flamePin, INPUT);
-  Serial.begin(9600);
+void setup() {
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(redled, OUTPUT);
+    pinMode(greenled, OUTPUT);
+    pinMode(flamePin, INPUT);
+    Serial.begin(9600);
 }
 
-void loop() 
-{
-  Flame = digitalRead(flamePin);
-  if (Flame== LOW)
-  {
-    Serial.println("ALL GOOD!");
-    digitalWrite(buzzerPin, LOW);
-    digitalWrite(redled, LOW);
-    digitalWrite(greenled, HIGH);
-  }
-  else
-  {
-    Serial.println("FIRE!!!!");
-    digitalWrite(buzzerPin, HIGH);
-    digitalWrite(greenled, LOW);
-    digitalWrite(redled, HIGH);
-  }
+void loop() {
+    Flame = digitalRead(flamePin);
+    if (Flame== LOW) {
+        Serial.println("ALL GOOD!");
+        digitalWrite(buzzerPin, LOW);
+        digitalWrite(redled, LOW);
+        digitalWrite(greenled, HIGH);
+    } else {
+        Serial.println("FIRE!!!!");
+        digitalWrite(buzzerPin, HIGH);
+        digitalWrite(greenled, LOW);
+        digitalWrite(redled, HIGH);
+    }
 }
